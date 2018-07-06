3ba785a9d559bcc928f5a0823909b98551296648
Wardormeur <wardormeur@gmail.com>
2018-07-06 13:29:40 +0100

    rename from dojo_lead_id to lead_id into dimDojos

--- etl/migrate.py
+++ etl/migrate.py
@@ -61,7 +61,7 @@ def migrate_db(dw_cursor, users_cursor, dojos_cursor, events_cursor):
                 inactive,
                 inactive_at,
                 is_eb,
-                dojo_lead_id)
+                lead_id)
             VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
         ''', map(transform_dojo, dojos_cursor.fetchall()))
         print("Inserted all dojos")
